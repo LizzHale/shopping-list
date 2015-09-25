c1be79d5fbb6c544fe7660baec551ad2dbf68347
Elizabeth Hale <lizzhale@mac.com>
2015-09-25 14:44:41 -0700

    Add it statments to cover some basic test cases

---
+++ test/test-server.js
@@ -0,0 +1,10 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+    it('should return 400 status if editing an item that does not exist');
+    it('should return 400 status code if deleting an item that does not exist'); ')
+    it('should delete a non-zero index item in the list');
+    it('should edit a non-zero index item in the list');
+})
